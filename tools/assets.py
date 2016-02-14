d2fb0ae469f55d5cfb2a708a93e55665a26ec1d2
郁骏 <ak64th@gmail.com>
2016-02-14 17:28:09 +0800

    use webassets to combine js files

---
+++ tools/assets.py
@@ -0,0 +1,21 @@
+import logging
+from webassets import Environment, Bundle
+from webassets.script import CommandLineEnvironment
+
+assets_env = Environment(directory='../public', url='/public')
+js = Bundle('bower_components/jquery/dist/jquery.min.js',
+            'bower_components/underscore/underscore-min.js',
+            'bower_components/backbone/backbone-min.js',
+            'script/constants.js', 'script/models.js', 'script/modal_view.js',
+            'script/pre_quiz_views.js', 'script/question_views.js',
+            'script/rank_view.js', 'script/quiz_views.js', 'script/main.js',
+            filters='jsmin', output='script/packed.js')
+assets_env.register('js_all', js)
+
+# Setup a logger
+log = logging.getLogger('webassets')
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+
+cmdenv = CommandLineEnvironment(assets_env, log)
+cmdenv.watch()
