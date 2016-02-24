8e769597bcdd359e1c6a45c8a064ed1284e4d936
郁骏 <ak64th@gmail.com>
2016-02-24 13:56:30 +0800

    fix counter

--- public/script/main.js
+++ public/script/main.js
@@ -1,44 +1,43 @@
-(function(root, $, _, Backbone){
+(function(root, $, _, Backbone) {
   app = root.app || {};
 
   app.ApplicationView = Backbone.View.extend({
     el: $('#app_container'),
     dataRoot: '/data/',
     apiRoot: '/rest/',
-    initialize: function(options){
+    initialize: function(options) {
       this.game_code = options.game_code;
       this.gameDataRoot = this.dataRoot + this.game_code + '/';
       this.quizConfig = options.config;
     },
-    run: function(){
+    run: function() {
       var errorView;
-      var used = parseInt(localStorage.getItem(this.game_code + '_count')),
-          max =  parseInt(this.quizConfig.max_chances);
-      if (used > max){
+      var used = parseInt(localStorage.getItem(this.game_code + '_count')) || 0,
+        max = parseInt(this.quizConfig.max_chances);
+      if (used > max) {
         errorView = new app.RejectionNoMoreChanceView();
       } else {
         var now = new Date(),
-            start_at = new Date(this.quizConfig.start_at),
-            end_at = new Date(this.quizConfig.end_at);
-        if(now < start_at){
+          start_at = new Date(this.quizConfig.start_at),
+          end_at = new Date(this.quizConfig.end_at);
+        if (now < start_at) {
           errorView = new app.RejectionTooEarlyView();
-        } else if (now > end_at){
+        } else if (now > end_at) {
           errorView = new app.RejectionTooLateView();
         }
       };
-      if(errorView){
+      if (errorView) {
         this.loadView(errorView);
       } else {
-        localStorage.setItem(this.game_code + '_count', used + 1);
-        application.prepareQuiz();
+        this.prepareQuiz();
       }
     },
-    loadView: function(view){
+    loadView: function(view) {
       this.view && (this.view.close ? this.view.close() : this.view.remove());
       this.view = view;
       this.$el.append(this.view.render().el);
     },
-    prepareQuiz: function(){
+    prepareQuiz: function() {
       var view = new app.WelcomeView({
         content: this.quizConfig.welcome,
         infoFields: this.quizConfig.info_fields,
@@ -47,7 +46,7 @@
       this.listenToOnce(view, 'startQuiz', this.startQuiz);
       this.loadView(view);
     },
-    startQuiz: function(args){
+    startQuiz: function(args) {
       // console.log(_.object(args));
       //Todo: send args to server
 
@@ -62,16 +61,19 @@
         case app.QUIZ_TYPE.CHALLENGE:
           ViewClass = app.ChallengeQuizView;
           break;
-        default: throw new Error('Unknown Quiz Type');
+        default:
+          throw new Error('Unknown Quiz Type');
       }
       var view = new ViewClass({
         config: this.quizConfig,
         gameDataRoot: this.gameDataRoot
       });
+      var used = parseInt(localStorage.getItem(this.game_code + '_count')) || 0;
+      localStorage.setItem(this.game_code + '_count', used + 1);
       this.listenToOnce(view, 'finishQuiz', this.finishQuiz);
       this.loadView(view);
     },
-    finishQuiz: function(){
+    finishQuiz: function() {
       console.log("结束");
       //Todo: send data to server
       var view = new app.RankView();
